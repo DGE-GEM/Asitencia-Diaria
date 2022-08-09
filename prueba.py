a3630860ff190b9729cf232123892db725637fd1
Andres Ruiz <98923276+AndresRuiz84@users.noreply.github.com>
2022-08-09 13:29:41 -0300

    09/08
    pocos cambios

--- prueba.py
+++ prueba.py
@@ -1,21 +1,7 @@
-import mysql.connector
+from multiprocessing import connection
+
+import pruebaConexion
+import pruebaManager
 import pruebaScripts
-from colorama import Fore
-from colorama import Style
 
-
-mydb = mysql.connector.connect
-
-
-  print("...iniciando conector a DB dbejemplo...")
-  
-  dbejemplo = mydb(
-    host="127.0.0.1",
-    user="root",
-    password="1234",
-    database="asistencia diaria"
-  )
-   
-
-
- 
+cursordb = pruebaManager.SetCursor(pruebaConexion.db)
